9ef0eb3dd9bb96800e90a8b7ed13cbc9b72de097
Leonardo <leonardouchoa.pedreira@gmail.com>
2021-11-08 18:19:47 -0300

    test pre-commit actions again

---
+++ long_function_def.py
@@ -0,0 +1,25 @@
+"""
+asdasd
+"""
+
+
+def very_long_function_name(
+    long_variable_name1,
+    long_variable_name2,
+    long_variable_name3,
+    long_variable_name4,
+    long_variable_name5,
+):
+    """
+    asd'
+    """
+    print(
+        long_variable_name1,
+        long_variable_name2,
+        long_variable_name3,
+        long_variable_name4,
+        long_variable_name5,
+    )
+
+
+very_long_function_name(1, 2, 3, 4, 5)
