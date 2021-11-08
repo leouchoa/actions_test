aa157beb4bc6cbd0a755475483e8ecbce4bb123a
Leonardo <leonardouchoa.pedreira@gmail.com>
2021-11-08 18:29:59 -0300

    add pytest github action file

---
+++ test_pytest_github_action.py
@@ -0,0 +1,38 @@
+"""
+asd
+"""
+
+
+def inc(x):
+    """
+    asd
+    """
+    return x + 1
+
+
+def test_answe1r():
+    """
+    asd
+    """
+    assert inc(3) == 5
+
+
+def test_answer2():
+    """
+    asd
+    """
+    assert inc(3) == 4
+
+
+def test_answer3():
+    """
+    asd
+    """
+    assert inc(4) == 5
+
+
+def test_answer4():
+    """
+    asd
+    """
+    assert inc(5) == 6
